29d278ad62ac911753a940b941e0a8827681d9e4
Alan Gatt <alangatt@gmail.com>
2019-04-19 07:18:41 +0200

    Styling Project Page

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 	<header class="flex items-center mb-3 py-4">
-		<div class="flex justify-between w-full items-center">
+		<div class="flex justify-between w-full items-end">
 			<h2 class="text-grey text-sm font-normal">My Projects</h2>
 			<a class="button" href="/projects/create">New Project</a>
 		</div>
@@ -11,14 +11,9 @@
 
 	<main class="lg:flex lg:flex-wrap -mx-3">
 		@forelse($projects as $project)
-		<div class="lg:w-1/3 px-3 pb-6">
-			<div class="bg-white rounded-lg shadow p-5" style="height: 200px;">
-				<h3 class="font-normal text-xl py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
-					<a href="{{$project->path()}}" class="text-black no-underline">{{$project->title}}</a>
-				</h3>
-				<div class="text-grey">{{str_limit($project->description,100)}}</div>
+			<div class="lg:w-1/3 px-3 pb-6">
+			@include('projects.cards')
 			</div>
-		</div>
 		@empty
 		<div>No projects yet!</div>
 		@endforelse
