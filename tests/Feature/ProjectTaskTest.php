45551e1564219341657ef1fe0bc8ee855c316597
Alan Gatt <alangatt@gmail.com>
2019-04-21 19:54:33 +0200

    Activity part 3
    Former-commit-id: d2615d0d9d265e129cc9a1628614a0fc3a127fdc

--- tests/Feature/ProjectTaskTest.php
+++ tests/Feature/ProjectTaskTest.php
@@ -51,6 +51,23 @@ class ProjectTaskTest extends TestCase
      
         $this->actingAs($project->owner)
             ->patch($project->tasks[0]->path(), [
+                'body' => 'changed'
+            ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed'
+        ]);
+
+    }
+
+    /** @test */
+    public function a_task_can_be_completed()
+    {
+        $project = ProjectFactory::withTasks(1)->create();
+        
+     
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
                 'body' => 'changed',
                 'completed' => true
             ]);
@@ -58,6 +75,31 @@ class ProjectTaskTest extends TestCase
         $this->assertDatabaseHas('tasks', [
             'body' => 'changed',
             'completed' => true
+        ]);
+
+    }
+
+    /** @test */
+    public function a_task_can_be_marked_as_incomplete()
+    {
+        $this->withoutExceptionHandling();
+        
+        $project = ProjectFactory::withTasks(1)->create();
+        
+        $this->actingAs($project->owner)
+            ->patch($project->tasks[0]->path(), [
+                'body' => 'changed',
+                'completed' => true
+            ]);
+        
+        $this->patch($project->tasks[0]->path(), [
+            'body' => 'changed',
+            'completed' => false
+        ]);
+        
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+            'completed' => false
         ]);
 
     }
