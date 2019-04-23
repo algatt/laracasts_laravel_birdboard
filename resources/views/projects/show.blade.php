582ba9dc8614d674b44e11bfdd4748210c2026b0
Alan Gatt <alangatt@gmail.com>
2019-04-23 21:55:24 +0200

    Gravatars
    Former-commit-id: e1f3fdc466ac9428b565888cfc378b44c1156574

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,7 +6,14 @@
 			<p class="text-grey text-sm font-normal">
 				<a href="/projects/" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{$project->title}}
 			</p>
-			<a class="button" href="{{$project->path().'/edit'}}">Edit Project</a>
+			<div class="flex items-center">
+				@foreach ($project->members as $member)
+					<img src="{{gravatar_url($member->email)}}" alt="{{$member->name}}'s avatar" class="rounded-full w-8 mr-2">
+				@endforeach
+				<img src="{{gravatar_url($project->owner->email)}}" alt="{{$project->owner->name}}'s avatar" class="rounded-full w-8 mr-2">
+				<a class="button ml-4" href="{{$project->path().'/edit'}}">Edit Project</a>
+			</div>
+			
 		</div>
 	</header>
 	
